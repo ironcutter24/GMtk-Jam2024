08ff8b67bc61398fbefc5116dd32b7a870ce5322
Giacomo Brugnoli <giacomobrugno@gmail.com>
2024-08-18 11:44:19 +0200

    Update fix ammo ShooterTrap

--- Assets/Scripts/Traps/Ammo_ShooterTrap.cs
+++ Assets/Scripts/Traps/Ammo_ShooterTrap.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Ammo_ShooterTrap : MonoBehaviour
 {
+    private void Start()
+    {
+        StartCoroutine(DestroyAfterABit());
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<PlayerController>().Death();
         }
+    }
+
+    IEnumerator DestroyAfterABit()
+    {
+        yield return new WaitForSeconds(5f);
+
+        Destroy(gameObject);
     }
 }
