388866fb957a355446d681831c2162d4ff5b51bd
Giacomo Brugnoli <giacomobrugno@gmail.com>
2024-08-20 18:29:11 +0200

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
             "Speed", new ParamValue[]
             {
                 new ParamValue(5f, Vector2.zero),
-                new ParamValue(10f, new Vector2(0f, 1f)),
+                new ParamValue(10f, new Vector2(0f, 2f)),
                 new ParamValue(15f, new Vector2(0f, 2f))
             }
         },
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
             {
                 new ParamValue(0f, Vector2.zero),
                 new ParamValue(1f, Vector2.zero),
-                new ParamValue(2f, new Vector2(1f, 0f))
+                new ParamValue(2f, new Vector2(.5f, 0f))
             }
         },
         {
@@ -48,7 +48,7 @@ public class PlayerController : MonoBehaviour
             {
                 new ParamValue(0f, Vector2.zero),
                 new ParamValue(1f, Vector2.zero),
-                new ParamValue(2f, new Vector2(2f, 2f))
+                new ParamValue(2f, new Vector2(.5f, 1f))
             }
         },
     };
